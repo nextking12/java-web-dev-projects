0ac5e2ad8b31910283ecc817dfbba4b2ba5421d8
Edward King <king8three@gmailcom>
2024-09-09 21:53:19 -0400

    practice with Studio

---
+++ control-flow-and-collections/studio/counting-characters/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class CountingCharacters {
+    public static void main(String[] args) {
+        HashMap<char[], Integer> mapChars = new HashMap<>();
+        String myString = "Hello, world!";
+        char[] charactersInString = myString.toCharArray();
+
+
+        }
+            //System.out.println(mapChars);
+        }
+        ;
