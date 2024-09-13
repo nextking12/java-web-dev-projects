b0dd99fb18b55653c2539b7f19a850ecd5ad5e16
Edward King <king8three@gmailcom>
2024-09-12 21:25:05 -0400

    worked on exercise

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Course {
+
+    String name;
+    String location;
+    Double grade;
+    HashMap<String, Double> nameGpa = new HashMap<>();
+
+
+
+
+}
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,12 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+
+
+    Student student = new Student("Jay", 12345, 1, 4.0);
+    System.out.println(student.getGpa());
+
+
+
     }
 }
